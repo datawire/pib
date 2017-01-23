a378e1d5b0a3b0c8c3727791aebc14730b0d5775
Itamar Turner-Trauring <itamar@datawire.io>
2017-01-23 10:34:14 -0500

    Load envfiles.

--- pib/envfile.py
+++ pib/envfile.py
@@ -1,12 +1,9 @@
 """Code for parsing the Envfile."""
 
-# from yaml import safe_load
+from yaml import safe_load
 from pyrsistent import PClass, field, pvector_field, pmap_field
 
-# from .schema import validate
-
-# Until we have a schema and can use, use an python-jsonschema-objects, just
-# use a thing that turns dictionaries into objects with attributes:
+from .schema import validate, ENVFILE_SCHEMA
 
 
 class DockerComponent(PClass):
@@ -19,7 +16,7 @@ class DockerComponent(PClass):
     port = field(type=int)
 
 
-class MinikubeEnvironment(PClass):
+class LocalDeployment(PClass):
     """Runs services and components on Minikube."""
     components = pvector_field(DockerComponent)
 
@@ -30,10 +27,25 @@ class RequiredComponent(PClass):
     template = field(type=str)
 
 
+class DockerImage(PClass):
+    """A Docker image."""
+    repository = field(type=str)
+    tag = field(type=str)
+
+
+class Expose(PClass):
+    """How a service should be exposed."""
+    # TODO: better structure
+    path = field(type=str)
+
+
 class Service(PClass):
     """A service."""
     name = field(type=str)
-    # image = ...
+    image = field(type=DockerImage)
+    port = field(type=int)
+    expose = field(type=Expose)
+    required_components = pmap_field(str, RequiredComponent)
 
 
 class Application(PClass):
@@ -44,11 +56,13 @@ class Application(PClass):
 
 class System(PClass):
     """A System loaded from an Envfile.yaml."""
-    environments = pvector_field(MinikubeEnvironment)
+    local_deployment = field(type=LocalDeployment)
+    remote_deployment = pmap_field(str, str)  # TODO: define structure
 
 
 def load_env_file(path_to_repo):
     """Load Envfile from path, return AttrDict."""
-    # with (path_to_repo / "Envfile.yaml").open() as f:
-    #    return AttrDict(safe_load(f.read()))
-    pass
+    with (path_to_repo / "Envfile.yaml").open() as f:
+        data = safe_load(f.read())
+    validate(ENVFILE_SCHEMA, data)
+    return System.create(safe_load(f.read()))
