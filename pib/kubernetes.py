6cdb71c989b984fb4fa40339f285440f99a8db6b
Itamar Turner-Trauring <itamar@datawire.io>
2017-01-23 16:25:38 -0500

    A higher-level representation as a dependency graph.

--- pib/kubernetes.py
+++ pib/kubernetes.py
@@ -1,39 +1,42 @@
 """Kubernetes integration for Pib."""
 
-from pyrsistent import PClass, field, pmap_field
+from pyrsistent import PClass, field, pset_field
+
+
+class AddressConfigMap(PClass):
+    """Kubernetes ConfigMap representation pointing at an InternalService.
+
+    This will be used to give a envfile service the addresses of envfile
+    deployments.
+    """
+    name = field(mandatory=True, type=str)
+    # TODO: When we support external services (e.g. AWS RDS) this may point at
+    # those as well:
+    backend_service = field(
+        mandatory=True, type="pib.kubernetes.InternalService")
+
+
+class Deployment(PClass):
+    """Kubernetes Deployment represenation."""
+    name = field(mandatory=True, type=str)
+    docker_image = field(mandatory=True, type=str)
+    port = field(mandatory=True, type=int)
+    replicas = field(mandatory=True, type=int)
+    address_configmaps = pset_field(AddressConfigMap)
+
+
+class InternalService(PClass):
+    """Kubernetes Service represenation.
+
+    This can represent either an envfile service or an envfile component.
+    """
+    name = field(mandatory=True, type=str)
+    type = field(mandatory=True, type=str)  # Either ClusterIP or NodePort
+    deployment = field(mandatory=True, type=Deployment)
 
 
 class Ingress(PClass):
     """Kubernetes Ingress representation."""
     name = field(mandatory=True, type=str)
     exposed_path = field(mandatory=True, type=str)
-    backend_service_name = field(mandatory=True, type=str)
-    backend_service_port = field(mandatory=True, type=int)
-
-
-class InternalService(PClass):
-    """Kubernetes Service represenation."""
-    name = field(mandatory=True, type=str)
-    labels = pmap_field(str, str)
-    selector = pmap_field(str, str)
-    exposed_port = field(mandatory=True, type=int)
-    target_port = field(mandatory=True, type=int)
-    type = field(mandatory=True, type=str)  # Either ClusterIP or NodePort
-
-
-class Deployment(PClass):
-    """Kubernetes Deployment represenation.
-
-    As a first pass we assume pods have same name and labels as the deployment.
-    """
-    name = field(mandatory=True, type=str)
-    labels = pmap_field(str, str)
-    docker_image = field(mandatory=True, type=str)
-    port = field(mandatory=True, type=int)
-    replicas = field(mandatory=True, type=int)
-
-
-class ConfigMap(PClass):
-    """Kubernetes ConfigMap representation."""
-    name = field(mandatory=True, type=str)
-    data = pmap_field(str, (int, str))
+    backend_service = field(mandatory=True, type=InternalService)
