c35879ec62d40dc9c4470b93b56295b03ee3d7d8
plombardi <plombardi89@gmail.com>
2017-01-27 04:42:47 -0500

    Initial work on the Terraform state file extractor. Handles RDS and Elasticsearch

---
+++ pib/tests/test_tfstatereader.py
@@ -0,0 +1,219 @@
+import pytest
+
+from ..tfstatereader import extract, ExtractedState
+
+INSTANCE = r"""
+{
+    "version": 3,
+    "terraform_version": "0.7.4",
+    "serial": 108,
+    "lineage": "79cc5f6a-b3b7-4b86-93e6-987ae7f373eb",
+    "remote": {
+        "type": "s3",
+        "config": {
+            "bucket": "datawire-terraform",
+            "key": "managed-infrastructure",
+            "region": "us-east-1"
+        }
+    },
+    "modules": [{
+        "path": [
+            "root",
+            "mcp_postgres_dev"
+        ],
+        "outputs": {},
+        "resources": {
+            "aws_db_instance.main": {
+                "type": "aws_db_instance",
+                "depends_on": [
+                    "aws_db_subnet_group.main"
+                ],
+                "primary": {
+                    "id": "mcp-develop",
+                    "attributes": {
+                        "address": "mcp-develop.crhykb8ijynb.us-east-1.rds.amazonaws.com",
+                        "allocated_storage": "10",
+                        "allow_major_version_upgrade": "true",
+                        "arn": "arn:aws:rds:us-east-1:914373874199:db:mcp-develop",
+                        "auto_minor_version_upgrade": "true",
+                        "availability_zone": "us-east-1c",
+                        "backup_retention_period": "0",
+                        "backup_window": "04:34-05:04",
+                        "copy_tags_to_snapshot": "false",
+                        "db_subnet_group_name": "mcp-develop",
+                        "endpoint": "mcp-develop.crhykb8ijynb.us-east-1.rds.amazonaws.com:5432",
+                        "engine": "postgres",
+                        "engine_version": "9.6.1",
+                        "id": "mcp-develop",
+                        "identifier": "mcp-develop",
+                        "instance_class": "db.t2.micro",
+                        "iops": "0",
+                        "kms_key_id": "",
+                        "license_model": "postgresql-license",
+                        "maintenance_window": "fri:05:42-fri:06:12",
+                        "monitoring_interval": "0",
+                        "multi_az": "false",
+                        "name": "",
+                        "option_group_name": "default:postgres-9-6",
+                        "parameter_group_name": "default.postgres9.6",
+                        "password": "REDACTED_PASSWORD",
+                        "port": "5432",
+                        "publicly_accessible": "false",
+                        "replicas.#": "0",
+                        "replicate_source_db": "",
+                        "security_group_names.#": "0",
+                        "skip_final_snapshot": "true",
+                        "status": "available",
+                        "storage_encrypted": "false",
+                        "storage_type": "gp2",
+                        "tags.%": "1",
+                        "tags.pib_metadata": "{ \"app\":\"datawire\", \"component_name\":\"postgres96\" }",
+                        "username": "REDACTED_USERNAME",
+                        "vpc_security_group_ids.#": "1",
+                        "vpc_security_group_ids.548992056": "sg-d43d90a9"
+                    },
+                    "meta": {},
+                    "tainted": false
+                },
+                "deposed": [],
+                "provider": ""
+            }
+        },
+        "depends_on": []
+    }, {
+        "path": [
+            "root",
+            "tracing_es_develop"
+        ],
+        "outputs": {
+            "elasticsearch_arn": {
+                "sensitive": false,
+                "type": "string",
+                "value": "arn:aws:es:us-east-1:914373874199:domain/tracing-develop"
+            },
+            "elasticsearch_domain_id": {
+                "sensitive": false,
+                "type": "string",
+                "value": "914373874199/tracing-develop"
+            },
+            "elasticsearch_endpoint": {
+                "sensitive": false,
+                "type": "string",
+                "value": "search-tracing-develop-oprqwhmnjzz75m6gy4eh3x6yqy.us-east-1.es.amazonaws.com"
+            }
+        },
+        "resources": {
+            "aws_elasticsearch_domain.elasticsearch": {
+                "type": "aws_elasticsearch_domain",
+                "depends_on": [
+                    "data.template_file.policy"
+                ],
+                "primary": {
+                    "id": "arn:aws:es:us-east-1:914373874199:domain/tracing-develop",
+                    "attributes": {
+                        "access_policies": "{\"Statement\":[{\"Action\":\"es:*\",\"Condition\":{\"IpAddress\":{\"aws:SourceIp\":\"0.0.0.0\"}},\"Effect\":\"Allow\",\"Principal\":\"*\",\"Resource\":\"arn:aws:es:us-east-1:914373874199:domain/tracing-develop/*\"}],\"Version\":\"2012-10-17\"}",
+                        "advanced_options.%": "1",
+                        "advanced_options.rest.action.multi.allow_explicit_index": "true",
+                        "arn": "arn:aws:es:us-east-1:914373874199:domain/tracing-develop",
+                        "cluster_config.#": "1",
+                        "cluster_config.0.dedicated_master_count": "2",
+                        "cluster_config.0.dedicated_master_enabled": "true",
+                        "cluster_config.0.dedicated_master_type": "t2.small.elasticsearch",
+                        "cluster_config.0.instance_count": "2",
+                        "cluster_config.0.instance_type": "t2.small.elasticsearch",
+                        "cluster_config.0.zone_awareness_enabled": "true",
+                        "domain_id": "914373874199/tracing-develop",
+                        "domain_name": "tracing-develop",
+                        "ebs_options.#": "1",
+                        "ebs_options.0.ebs_enabled": "true",
+                        "ebs_options.0.iops": "0",
+                        "ebs_options.0.volume_size": "20",
+                        "ebs_options.0.volume_type": "gp2",
+                        "elasticsearch_version": "2.3",
+                        "endpoint": "search-tracing-develop-oprqwhmnjzz75m6gy4eh3x6yqy.us-east-1.es.amazonaws.com",
+                        "id": "arn:aws:es:us-east-1:914373874199:domain/tracing-develop",
+                        "snapshot_options.#": "1",
+                        "snapshot_options.0.automated_snapshot_start_hour": "0",
+                        "tags.%": "1",
+                        "tags.pib_metadata": "{ \"app\":\"datawire\", \"service\":\"trace\", \"component_name\":\"es\" }"
+                    },
+                    "meta": {},
+                    "tainted": false
+                },
+                "deposed": [],
+                "provider": ""
+            }
+        },
+        "depends_on": []
+    }, {
+        "path": [
+            "root",
+            "tracing_es_prod"
+        ],
+        "outputs": {
+            "elasticsearch_arn": {
+                "sensitive": false,
+                "type": "string",
+                "value": "arn:aws:es:us-east-1:914373874199:domain/tracing-prod"
+            },
+            "elasticsearch_domain_id": {
+                "sensitive": false,
+                "type": "string",
+                "value": "914373874199/tracing-prod"
+            },
+            "elasticsearch_endpoint": {
+                "sensitive": false,
+                "type": "string",
+                "value": "search-tracing-prod-jtw3midcw72dvqpciz3mufifkm.us-east-1.es.amazonaws.com"
+            }
+        },
+        "resources": {
+            "aws_elasticsearch_domain.elasticsearch": {
+                "type": "aws_elasticsearch_domain",
+                "depends_on": [
+                    "data.template_file.policy"
+                ],
+                "primary": {
+                    "id": "arn:aws:es:us-east-1:914373874199:domain/tracing-prod",
+                    "attributes": {
+                        "access_policies": "{\"Statement\":[{\"Action\":\"es:*\",\"Condition\":{\"IpAddress\":{\"aws:SourceIp\":\"0.0.0.0\"}},\"Effect\":\"Allow\",\"Principal\":\"*\",\"Resource\":\"arn:aws:es:us-east-1:914373874199:domain/tracing-prod/*\"}],\"Version\":\"2012-10-17\"}",
+                        "advanced_options.%": "1",
+                        "advanced_options.rest.action.multi.allow_explicit_index": "true",
+                        "arn": "arn:aws:es:us-east-1:914373874199:domain/tracing-prod",
+                        "cluster_config.#": "1",
+                        "cluster_config.0.dedicated_master_count": "2",
+                        "cluster_config.0.dedicated_master_enabled": "true",
+                        "cluster_config.0.dedicated_master_type": "m3.medium.elasticsearch",
+                        "cluster_config.0.instance_count": "2",
+                        "cluster_config.0.instance_type": "m3.medium.elasticsearch",
+                        "cluster_config.0.zone_awareness_enabled": "true",
+                        "domain_id": "914373874199/tracing-prod",
+                        "domain_name": "tracing-prod",
+                        "ebs_options.#": "1",
+                        "ebs_options.0.ebs_enabled": "true",
+                        "ebs_options.0.iops": "0",
+                        "ebs_options.0.volume_size": "20",
+                        "ebs_options.0.volume_type": "gp2",
+                        "elasticsearch_version": "2.3",
+                        "endpoint": "search-tracing-prod-jtw3midcw72dvqpciz3mufifkm.us-east-1.es.amazonaws.com",
+                        "id": "arn:aws:es:us-east-1:914373874199:domain/tracing-prod",
+                        "snapshot_options.#": "1",
+                        "snapshot_options.0.automated_snapshot_start_hour": "0",
+                        "tags.%": "0"
+                    },
+                    "meta": {},
+                    "tainted": false
+                },
+                "deposed": [],
+                "provider": ""
+            }
+        },
+        "depends_on": []
+    }]
+}
+"""
+
+
+def test_extract():
+    extracted = extract(INSTANCE)
+    assert 2 == extracted.size()
---
+++ pib/tfstatereader.py
@@ -0,0 +1,164 @@
+import json
+
+
+class Metadata:
+
+    def __init__(self, app, service, component_name):
+        self.app = app
+        self.service = service
+        self.component_name = component_name
+
+
+class Injectable:
+
+    def __init__(self, metadata, config):
+        for k, v in config.items():
+            del config[k]
+            config[metadata.component_name + '_COMPONENT' + k] = v
+
+        self.metadata = metadata
+
+    def config(self):
+        return self.config
+
+
+class AwsElasticsearchDomain(Injectable):
+
+    def __init__(self, metadata, config):
+        Injectable.__init__(self, metadata, config)
+
+    @staticmethod
+    def create(pib_metadata, tf_data):
+        config = {'HOST': tf_data['attributes']['endpoint'],
+                  'PORT': ""}  # doesn't have one...
+
+        return AwsElasticsearchDomain(pib_metadata, config)
+
+
+class AwsDatabaseResource(Injectable):
+
+    def __init__(self, metadata, config):
+        Injectable.__init__(self, metadata, config)
+
+    @staticmethod
+    def create(pib_metadata, tf_data):
+        config = {'HOST': tf_data['attributes']['address'],
+                  'PORT': tf_data['attributes']['port'],
+                  'USERNAME': tf_data['attributes']['username'],
+                  'PASSWORD': tf_data['attributes']['password']}
+
+        return AwsDatabaseResource(pib_metadata, config)
+
+
+class ExtractedState:
+
+    def __init__(self):
+        self.all = []
+        self.app_resources = {}
+        self.svc_resources = {}
+
+    def add_resource(self, resource):
+        self.all.append(resource)
+
+        if resource.metadata.app not in self.app_resources:
+            self.app_resources[resource.metadata.app] = []
+
+        self.app_resources[resource.metadata.app].append(resource)
+
+        print(resource.metadata.app)
+        print(resource.metadata.service)
+        print(resource.metadata.component_name)
+
+        # resource.service can be None which means it's shared and therefore doesn't belong to any particular resource.
+        if resource.metadata.service is not None and resource.metadata.service not in self.svc_resources:
+            self.svc_resources[resource.metadata.service] = []
+
+        if resource.metadata.service is not None:
+            self.svc_resources[resource.metadata.service].append(resource)
+
+    def clear(self):
+        self.all.clear()
+        self.app_resources.clear()
+        self.svc_resources.clear()
+
+    def size(self):
+        return len(self.all)
+
+    def render(self, renderer):
+        renderer.render(self)
+
+RESOURCE_FACTORIES = {
+    'aws_db_instance': AwsDatabaseResource.create,
+    'aws_elasticsearch_domain': AwsElasticsearchDomain.create
+}
+
+
+def extract(raw_json):
+    """Terraform stores state in a convenient-ish JSON format. This """
+
+    # def flatten_hook(obj):
+    #     for key, value in obj.items():
+    #         if isinstance(value, str):
+    #             try:
+    #                 obj[key] = json.loads(value, object_hook=flatten_hook)
+    #             except ValueError:
+    #                 pass
+    #     return obj
+
+    tfstate = json.loads(raw_json)
+    extracted = ExtractedState()
+
+    for mod in tfstate['modules']:
+        extract_resources_from_module(extracted, mod)
+
+    return extracted
+
+
+def extract_resources_from_module(result, mod):
+
+    # module.resources is a dictionary that maps the Terraform templates resource name to data about that resource. We
+    # are not interested in that value. The interesting info lies in the tf_data dictionary.
+    for tf_name, tf_data in mod.get('resources', {}).items():
+
+        # there's a huge number of Terraform resources we can't do anything intelligent with.
+        if tf_data['type'] not in RESOURCE_FACTORIES:
+            print("SKIP: no type handle for {}".format(tf_data['type']))
+            continue
+
+        # TODO(plombardi): INVESTIGATE
+        # there's a 'primary' and a 'deposed'... I'm not sure what deposed is or if it's relevant to anything. Primary
+        # data is the stuff we're after.
+        primary = tf_data['primary']
+
+        # tainted stuff is going to be destroyed by Terraform so do not do anything with it.
+        if primary['tainted']:
+            print("SKIP: tainted resourced")
+            continue
+
+        attributes = primary['attributes']
+        tags = extract_tags(attributes)
+
+        # The metadata holds app and service name information. We use a JSON object to store that information to avoid
+        # wasting AWS resource tags because each resource can only have a maximum of 10. If a resource does not have
+        # metadata it's not meant to be consumed.
+        raw_metadata = json.loads(tags.get('pib_metadata', '{}'))
+        if not raw_metadata:
+            print("SKIP: no metadata")
+            continue
+
+        metadata = Metadata(raw_metadata['app'],
+                            raw_metadata.get('service'),
+                            raw_metadata['component_name'])
+
+        injectable = RESOURCE_FACTORIES[tf_data['type']](metadata, primary)
+        result.add_resource(injectable)
+
+
+def extract_tags(attributes):
+    result = {}
+    for k, v in attributes.items():
+        # no idea why they seem to use .# and .% at different types or if means something...
+        if k.startswith('tags.') and k not in {'tags.#', 'tags.%'}:
+            result[k.split('.', 1)[-1]] = v
+
+    return result
